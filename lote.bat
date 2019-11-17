140b7306b2a49d5d99bb950dfb1d30f0ad00e5f3
Gadhy <56990320+gadhy@users.noreply.github.com>
2019-11-17 12:45:20 -0500

    Presentacion del 6to trabajo

--- lote.bat
+++ lote.bat
@@ -1,1 +1,123 @@
-python validador1.py 3.14 2 5
+python zena\condiciones_simples\validador1.py 3.14 2 5
+python zena\condiciones_simples\validador1.py 3.14 4 6
+python zena\condiciones_simples\validador1.py 3.14 3 7
+
+python zena\condiciones_simples\validador2.py "JUAN" "PEDRO" 4 30
+python zena\condiciones_simples\validador2.py "DIEGO" "LEHO" 5 20
+python zena\condiciones_simples\validador2.py "MANUEL" "ULISES" 2 60
+
+python zena\condiciones_simples\validador3.py 5 1.5 30
+python zena\condiciones_simples\validador3.py 4 1.6 40
+python zena\condiciones_simples\validador3.py 3 1.8 50
+
+python zena\condiciones_simples\validador4.py "JOSE" "ERICK" 4 18.5
+python zena\condiciones_simples\validador4.py "EMANUEL" "FERNANDO" 2 19.5
+python zena\condiciones_simples\validador4.py "GENNER" "SNAIDER" 3 20.5
+
+python zena\condiciones_simples\validador5.py 3.14 2 10
+python zena\condiciones_simples\validador5.py 3.14 3 15
+python zena\condiciones_simples\validador5.py 3.14 4 20
+
+python zena\condiciones_simples\validador6.py "YERICK" "MAGALY" 2 50
+python zena\condiciones_simples\validador6.py "LIAM" "MELISSA" 3 100
+python zena\condiciones_simples\validador6.py "BRUNO" "DIANA" 4 30
+
+python zena\condiciones_simples\validador7.py "BENIGNO" "NANCY" 3 3.6
+python zena\condiciones_simples\validador7.py "DANIEL" "GABRIEL" 4 2.5
+python zena\condiciones_simples\validador7.py "DAVID" "MARTIN" 2 4.1
+
+python zena\condiciones_simples\validador8.py 4 9.8 6
+python zena\condiciones_simples\validador8.py 5 9.8 2
+python zena\condiciones_simples\validador8.py 3 9.8 4
+
+python zena\condiciones_simples\validador9.py 15 3 4
+python zena\condiciones_simples\validador9.py 16 2 3
+python zena\condiciones_simples\validador9.py 12 4 5
+
+python zena\condiciones_simples\validador10.py 12
+python zena\condiciones_simples\validador10.py 15
+python zena\condiciones_simples\validador10.py 10
+
+
+
+python zena\condiciones_dobles\validadordoble1.py 3.14 2 6
+python zena\condiciones_dobles\validadordoble1.py 3.14 3 8
+python zena\condiciones_dobles\validadordoble1.py 3.14 4 4
+
+python zena\condiciones_dobles\validadordoble2.py "JUAN" "PEDRO" 4 30
+python zena\condiciones_dobles\validadordoble2.py "PABLO" "TIAGO" 3 20
+python zena\condiciones_dobles\validadordoble2.py "RAMON" "RICHAR" 2 40
+
+python zena\condiciones_dobles\validadordoble3.py 5 1.5 30
+python zena\condiciones_dobles\validadordoble3.py 4 1.6 31
+python zena\condiciones_dobles\validadordoble3.py 6 1.8 32
+
+python zena\condiciones_dobles\validadordoble4.py "NICOLA" "YACO" 4 18.5
+python zena\condiciones_dobles\validadordoble4.py "EMA" "FELYX" 2 19.5
+python zena\condiciones_dobles\validadordoble4.py "GENARO" "SANDRO" 3 20.5
+
+python zena\condiciones_dobles\validadordoble5.py 3.14 3 12
+python zena\condiciones_dobles\validadordoble5.py 3.14 4 14
+python zena\condiciones_dobles\validadordoble5.py 3.14 2 21
+
+python zena\condiciones_dobles\validadordoble6.py "EDGARDO" "MARIA" 2 50
+python zena\condiciones_dobles\validadordoble6.py "LIAM" "MARTHA" 3 100
+python zena\condiciones_dobles\validadordoble6.py "FACUNDO" "ADAMARY" 4 30
+
+python zena\condiciones_dobles\validadordoble7.py "BENITO" "MARLENY" 3 3.6
+python zena\condiciones_dobles\validadordoble7.py "DANIEL" "MARIO" 4 2.5
+python zena\condiciones_dobles\validadordoble7.py "DAVID" "JAVIER" 2 4.1
+
+python zena\condiciones_dobles\validadordoble8.py  4 9.8 6
+python zena\condiciones_dobles\validadordoble8.py  2 9.8 8
+python zena\condiciones_dobles\validadordoble8.py  3 9.8 7
+
+python zena\condiciones_dobles\validadordoble9.py 100 3 4
+python zena\condiciones_dobles\validadordoble9.py 33 2 3
+python zena\condiciones_dobles\validadordoble9.py 150 1 5
+
+python zena\condiciones_dobles\validadordoble10.py 13
+python zena\condiciones_dobles\validadordoble10.py 16
+python zena\condiciones_dobles\validadordoble10.py 17
+
+
+
+python zena\condiciones_multiples\validadormultiple1.py 3.14 3 6
+python zena\condiciones_multiples\validadormultiple1.py 3.14 2 7
+python zena\condiciones_multiples\validadormultiple1.py 3.14 4 2
+
+python zena\condiciones_multiples\validadormultiple2.py "ERLAND" "NOLBERT" 3 30
+python zena\condiciones_multiples\validadormultiple2.py "SALVADOR" "FRANK" 2 15
+python zena\condiciones_multiples\validadormultiple2.py "LENY" "HILDER" 4 20
+
+python zena\condiciones_multiples\validadormultiple3.py 5 1.7 28
+python zena\condiciones_multiples\validadormultiple3.py 4 1.1 29
+python zena\condiciones_multiples\validadormultiple3.py 3 1.2 31
+
+python zena\condiciones_multiples\validadormultiple4.py "JORGE" "ESTEBAN" 4 17.5
+python zena\condiciones_multiples\validadormultiple4.py "FEDERICO" "ROBERTO" 3 18.4
+python zena\condiciones_multiples\validadormultiple4.py "CRISTHIAM" "FREDY" 5 18.3
+
+python zena\condiciones_multiples\validadormultiple5.py 3.14 3 9
+python zena\condiciones_multiples\validadormultiple5.py 3.14 2 11
+python zena\condiciones_multiples\validadormultiple5.py 3.14 1 12
+
+python zena\condiciones_multiples\validadormultiple6.py "RAUL" "TANIA" 2 50
+python zena\condiciones_multiples\validadormultiple6.py "GREGORIO" "MILAGROS" 3 49
+python zena\condiciones_multiples\validadormultiple6.py "RUBEN" "YEDIT" 1 51
+
+python zena\condiciones_multiples\validadormultiple7.py "JESUS" "NICOLE" 3 3.6
+python zena\condiciones_multiples\validadormultiple7.py "BERNARDOR" "SUNYI" 4 2.6
+python zena\condiciones_multiples\validadormultiple7.py "OCTAVIO" "YUNNY" 2 4.6
+
+python zena\condiciones_multiples\validadormultiple8.py 6 9.8 4
+python zena\condiciones_multiples\validadormultiple8.py 5 9.8 5
+python zena\condiciones_multiples\validadormultiple8.py 4 9.8 7
+
+python zena\condiciones_multiples\validadormultiple9.py 20 3 6
+python zena\condiciones_multiples\validadormultiple9.py 100 2 5
+python zena\condiciones_multiples\validadormultiple9.py 60 1 4
+
+python zena\condiciones_multiples\validadormultiple10.py 14
+python zena\condiciones_multiples\validadormultiple10.py 15
+python zena\condiciones_multiples\validadormultiple10.py 12
